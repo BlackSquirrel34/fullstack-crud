3eaa3c8608b3afbe3dce301b5b2a836999459e35
BlackSquirrel <Maexxla3458@proton.me>
2025-12-06 18:46:40 +0100

    navbar, darkmode toggle

---
+++ fullstack_crud/src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { HomeIcon, Sprout } from "lucide-react";
+import ModeToggle from "./ModeToggle";
+
+function Navbar() {
+  return (
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg.background/60 z-50">
+      <div className="mx-w-7xl mx-auto px-4">
+        <div className="flex items-center h-16 justify-between">
+          {/* Logo */}
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className="text-cl font-bold text-primary font-mono tracking-wider"
+            >
+              🌱 Plantventory
+            </Link>
+          </div>
+
+          {/*  Navbar components */}
+          <div className="hidden md:flex items-center space-x-4">
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link href="/plants">
+                <Sprout className="w-4 h-4" />
+                <span className="hidden lg:inline">Plants</span>
+              </Link>
+            </Button>
+
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link href="/">
+                <HomeIcon className="w-4 h-4" />
+                <span className="hidden lg:inline">Home</span>
+              </Link>
+            </Button>
+
+            <ModeToggle />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
