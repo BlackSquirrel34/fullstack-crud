189fb9b494d54bac8d0ec3b7373670be0a598340
BlackSquirrel <Maexxla3458@proton.me>
2025-12-06 20:12:35 +0100

    ui with hero, table for /plants

--- fullstack_crud/src/app/page.tsx
+++ fullstack_crud/src/app/page.tsx
@@ -1,10 +1,7 @@
+import Hero from "@/components/Hero";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Click me</Button>
-    </div>
-  );
+  return <Hero />;
 }
---
+++ fullstack_crud/src/app/plants/page.tsx
@@ -0,0 +1,28 @@
+import DemoTable from "@/components/DemoTable";
+import InventoryTable from "@/components/InventoryTable";
+import { stackServerApp } from "@/stack/server";
+import { SignUp } from "@stackframe/stack";
+import React from "react";
+
+async function PlantPage() {
+  const user = await stackServerApp.getUser();
+  const app = stackServerApp.urls;
+
+  return (
+    <>
+      {user ? (
+        <div className="mt-7 max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-10 gap-6">
+          <div className="lg:col-span-full">
+            <InventoryTable />
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-center mt-20 items-center">
+          <SignUp />
+        </div>
+      )}
+    </>
+  );
+}
+
+export default PlantPage;
